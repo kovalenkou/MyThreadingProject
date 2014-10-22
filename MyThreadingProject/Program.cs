461eee9eb90421561c3a0e9c86c76b1a762c0487
kovalenkou <anekosheik@gmail.com>
2014-10-22 17:58:49 +0300

    add file Class1.cs with class-work fro threading

--- MyThreadingProject/Program.cs
+++ MyThreadingProject/Program.cs
@@ -6,107 +6,107 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace MyThreadingProject
-{
-    class Test
-    {
-        public static void Next(object num)
-        {
-            int n = (int)num;
-            Console.WriteLine("Поток № {0} запущен", n);
-            //Thread.Sleep(1000);
-            int res = 0;
-            bool? flag;
-            Queue<bool?> q = new Queue<bool?>();
-            do
-            {
-                res = new Random().Next(0, 500);
-                res %= 11;
-                switch (res)
-                {
-                    case 1:
-                        {
-                            flag = false;
-                            break;
-                        }
-                    case 3:
-                        {
-                            flag = true;
-                            break;
-                        }
-                    default:
-                        {
-                            flag = null;
-                            break;
-                        }
-                }
-                if (flag != null)
-                {
-                    q.Enqueue(flag);
-                }
-            } while (flag != null);
-            Console.WriteLine("++++++++++++++");
-            foreach (bool? f in q)
-                Console.WriteLine(f);
-            Console.WriteLine("---------------");
-            Console.WriteLine("Завершен поток № {0}", n);
-        }
+//namespace MyThreadingProject
+//{
+    //class Test
+    //{
+    //    public static void Next(object num)
+    //    {
+    //        int n = (int)num;
+    //        Console.WriteLine("Поток № {0} запущен", n);
+    //        //Thread.Sleep(1000);
+    //        int res = 0;
+    //        bool? flag;
+    //        Queue<bool?> q = new Queue<bool?>();
+    //        do
+    //        {
+    //            res = new Random().Next(0, 500);
+    //            res %= 11;
+    //            switch (res)
+    //            {
+    //                case 1:
+    //                    {
+    //                        flag = false;
+    //                        break;
+    //                    }
+    //                case 3:
+    //                    {
+    //                        flag = true;
+    //                        break;
+    //                    }
+    //                default:
+    //                    {
+    //                        flag = null;
+    //                        break;
+    //                    }
+    //            }
+    //            if (flag != null)
+    //            {
+    //                q.Enqueue(flag);
+    //            }
+    //        } while (flag != null);
+    //        Console.WriteLine("++++++++++++++");
+    //        foreach (bool? f in q)
+    //            Console.WriteLine(f);
+    //        Console.WriteLine("---------------");
+    //        Console.WriteLine("Завершен поток № {0}", n);
+    //    }
 
-        public static void HCFunk(object num)
-        {
-            int n = (int)num;
-            for (int i = 0; i < 5; i++)
-            {
-                //Console.WriteLine("Поток № {0} запущен", n);
+    //    public static void HCFunk(object num)
+    //    {
+    //        int n = (int)num;
+    //        for (int i = 0; i < 5; i++)
+    //        {
+    //            //Console.WriteLine("Поток № {0} запущен", n);
 
-                Console.WriteLine("Поток № {0} вернул HashCode {1}", n, Thread.CurrentThread.GetHashCode().ToString());
-                //Thread.Sleep(0);
-                //Console.WriteLine("Завершен поток № {0}", n);
-            }
-        }
+    //            Console.WriteLine("Поток № {0} вернул HashCode {1}", n, Thread.CurrentThread.GetHashCode().ToString());
+    //            //Thread.Sleep(0);
+    //            //Console.WriteLine("Завершен поток № {0}", n);
+    //        }
+    //    }
 
-        static double res = 25.0;
-        static double muneric = 5.0;
-        static double pownum = 1.0;
+    //    static double res = 25.0;
+    //    static double muneric = 5.0;
+    //    static double pownum = 1.0;
 
-        public static void Num()
-        {
-            Thread.Sleep(500);
-            muneric = new Random().Next(1, 10);
-            return;
-        }
+    //    public static void Num()
+    //    {
+    //        Thread.Sleep(500);
+    //        muneric = new Random().Next(1, 10);
+    //        return;
+    //    }
 
-        public static void Pow()
-        {
-            Thread.Sleep(500);
-            pownum = new Random().Next(1, 5);
-            return;
-        }
+    //    public static void Pow()
+    //    {
+    //        Thread.Sleep(500);
+    //        pownum = new Random().Next(1, 5);
+    //        return;
+    //    }
 
-        public static void Result()
-        {
-            res = Math.Pow(muneric, pownum);
-            Console.WriteLine("результат: {0}", res);
-        }
+    //    public static void Result()
+    //    {
+    //        res = Math.Pow(muneric, pownum);
+    //        Console.WriteLine("результат: {0}", res);
+    //    }
 
-        public static object locker = new object();
+    //    public static object locker = new object();
 
-        public static void Go()
-        {
-            lock (locker)
-            {
+    //    public static void Go()
+    //    {
+    //        lock (locker)
+    //        {
  
-                Console.WriteLine(res / muneric);
-                Thread.Sleep(1000);
-            }
-        }
+    //            Console.WriteLine(res / muneric);
+    //            Thread.Sleep(1000);
+    //        }
+    //    }
 
-    }
+    //}
     
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
 //--- проверка на LOCK
             //new Thread(Test.Go).Start();
             //new Thread(Test.Go).Start();
@@ -166,6 +166,6 @@ namespace MyThreadingProject
 
 
 
-        }
-    }
-}
+//        }
+//    }
+//}
